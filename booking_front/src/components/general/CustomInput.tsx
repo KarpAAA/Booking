ae41817ca9366fcc78c42fdbd5d287609dd17746
Ivan Karpinskyi <ivankarpinskij.ua@gmail.com>
2024-05-14 21:05:16 +0300

    Dockerize app

--- booking_front/src/components/general/CustomInput.tsx
+++ booking_front/src/components/general/CustomInput.tsx
@@ -13,6 +13,7 @@ export const CustomInput: FC<CustomInputProps> = ({labelText, ...other}) => {
             <label className={'font-bold'}>{labelText}</label>
             <input
                 {...other}
+                minLength={8}
                 className={'px-2 py-2 rounded w-full border border-solid border-black text-black bg-white text-lg mt-1'}/>
 
         </div>
